133f6dbfca0cfeef7c44961aa1428d27c5a05b89
Skirmantas Jurgaitis <skyjur@gmail.com>
2023-01-14 01:10:20 +0100

    add server logging

--- server.js
+++ server.js
@@ -12,24 +12,27 @@ const promise = r => new Promise((resolve, reject) => {
 
 const server = http.createServer(async (req, res) => {
   const {url} = req
+  console.log(`${req.socket.remoteAddress}`, req.method, url)
 
   if(url === '/') {
     const indexData = await promise(r => 
       fs.readFile('index.html', {'encoding': 'utf-8'}, r))
     indexData && res.write(indexData)
     res.end()
-  } else if(url === '/data/100mb') {
-    const bytes = 1024 * 1024 * 100
+  } else if(url === '/data/10mb') {
+    const bytes = 1024 * 1024 * 10
     res.statusCode = 200
     res.setHeader('Content-Length', bytes.toFixed())
     res.flushHeaders()
     res.write(createData(bytes))
     res.end()
   } else {
-    res.statusCode = 200
+    res.statusCode = 404
     res.write('404')
     res.end()
   }
 })
 
 server.listen(1000)
+
+console.log('Ready')
